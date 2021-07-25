8d68e1517c15f857456f7e13cfd369da5ef3cf59
Junichi Nishi <github@purewest.dip.jp>
2021-07-26 08:55:04 +0900

    TaskFormクラスを追加

--- todo_app/forms.py
+++ todo_app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Folder
+from .models import Folder, Task#Taskモデルをインポート
 
 class FolderForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
@@ -12,3 +12,21 @@ class FolderForm(forms.ModelForm):
         model = Folder
         fields = ('title',)
         labels = {'title' : 'フォルダー名'}
+
+
+class TaskForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(TaskForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs = {
+                'class': 'form-control'
+            }
+    class Meta:
+        STATUS_CHOICES = [(1, '未完了'),(2, '作業中'),(3, '完了')]
+        model = Task
+        fields = ('title', 'status','due_date')
+        labels = {
+            'title': 'タスク名',
+            'status': '状態',
+            'due_date': '期限',
+        }
