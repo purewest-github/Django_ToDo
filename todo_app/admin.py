84b09840634fd2e64a97b2ecb8b180f44e4e0613
Junichi Nishi <github@purewest.dip.jp>
2021-07-26 07:22:06 +0900

    Tasksの項目を追加

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Folder
+from .models import Folder, Task#追加
 
 # Register your models here.
 admin.site.register(Folder)
+admin.site.register(Task)#追加
+
 
